16d04868d90a8e1b4a0170f2e200cdf318e58fe8
paula <cruz.amp12@gmail.com>
2019-05-03 16:41:44 -0700

    sum commit

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
+
+
+
+
 /////////////////////////////////////
-/* Problem 2
+/* Problem 2 
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
@@ -29,13 +33,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  var output = a * b;
+  var message = 'The sum of ' + a + ' and ' + b + ' is ' + output + ',';
+  return [output, message];
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+
+
 
 /////////////////////////////////////
 /* Problem 3
